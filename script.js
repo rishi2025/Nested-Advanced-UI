80cb58b176e9fe492f793d1aab12f3c95ecbac5b
rishi2025 <99127485+rishi2025@users.noreply.github.com>
2023-08-27 10:57:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -43,41 +43,41 @@ closePopUp.forEach(btn => btn.addEventListener('click', closeModal))
 
 
 
-// document.querySelector('.buttonTwo').addEventListener('click', function () {
-//     console.log("clicked");
-//     document.querySelector('.buttonTwo').classList.remove('activeButton');
-//     document.querySelector('.buttonOne').classList.remove('activeButton');
-//     document.querySelector('.buttonThree').classList.remove('activeButton');
-//     document.querySelector('.buttonTwo').classList.add('activeButton');
-//     document.querySelector('.containerDataTwo').classList.add('inactiveContainer');
-//     document.querySelector('.containerDataOne').classList.add('inactiveContainer');
-//     document.querySelector('.containerDataThree').classList.add('inactiveContainer');
-//     document.querySelector('.containerDataTwo').classList.remove('inactiveContainer');
-// });
+document.querySelector('.buttonTwo').addEventListener('click', function () {
+    console.log("clicked");
+    document.querySelector('.buttonTwo').classList.remove('activeButton');
+    document.querySelector('.buttonOne').classList.remove('activeButton');
+    document.querySelector('.buttonThree').classList.remove('activeButton');
+    document.querySelector('.buttonTwo').classList.add('activeButton');
+    document.querySelector('.containerDataTwo').classList.add('inactiveContainer');
+    document.querySelector('.containerDataOne').classList.add('inactiveContainer');
+    document.querySelector('.containerDataThree').classList.add('inactiveContainer');
+    document.querySelector('.containerDataTwo').classList.remove('inactiveContainer');
+});
 
-// document.querySelector('.buttonThree').addEventListener('click', function () {
-//     console.log("clicked");
-//     document.querySelector('.buttonTwo').classList.remove('activeButton');
-//     document.querySelector('.buttonOne').classList.remove('activeButton');
-//     document.querySelector('.buttonThree').classList.remove('activeButton');
-//     document.querySelector('.buttonThree').classList.add('activeButton');
-//     document.querySelector('.containerDataTwo').classList.add('inactiveContainer');
-//     document.querySelector('.containerDataOne').classList.add('inactiveContainer');
-//     document.querySelector('.containerDataThree').classList.add('inactiveContainer');
-//     document.querySelector('.containerDataThree').classList.remove('inactiveContainer');
-// });
+document.querySelector('.buttonThree').addEventListener('click', function () {
+    console.log("clicked");
+    document.querySelector('.buttonTwo').classList.remove('activeButton');
+    document.querySelector('.buttonOne').classList.remove('activeButton');
+    document.querySelector('.buttonThree').classList.remove('activeButton');
+    document.querySelector('.buttonThree').classList.add('activeButton');
+    document.querySelector('.containerDataTwo').classList.add('inactiveContainer');
+    document.querySelector('.containerDataOne').classList.add('inactiveContainer');
+    document.querySelector('.containerDataThree').classList.add('inactiveContainer');
+    document.querySelector('.containerDataThree').classList.remove('inactiveContainer');
+});
 
-// document.querySelector('.buttonOne').addEventListener('click', function () {
-//     console.log("clicked");
-//     document.querySelector('.buttonTwo').classList.remove('activeButton');
-//     document.querySelector('.buttonOne').classList.remove('activeButton');
-//     document.querySelector('.buttonThree').classList.remove('activeButton');
-//     document.querySelector('.buttonOne').classList.add('activeButton');
-//     document.querySelector('.containerDataTwo').classList.add('inactiveContainer');
-//     document.querySelector('.containerDataOne').classList.add('inactiveContainer');
-//     document.querySelector('.containerDataThree').classList.add('inactiveContainer');
-//     document.querySelector('.containerDataOne').classList.remove('inactiveContainer');
-// });
+document.querySelector('.buttonOne').addEventListener('click', function () {
+    console.log("clicked");
+    document.querySelector('.buttonTwo').classList.remove('activeButton');
+    document.querySelector('.buttonOne').classList.remove('activeButton');
+    document.querySelector('.buttonThree').classList.remove('activeButton');
+    document.querySelector('.buttonOne').classList.add('activeButton');
+    document.querySelector('.containerDataTwo').classList.add('inactiveContainer');
+    document.querySelector('.containerDataOne').classList.add('inactiveContainer');
+    document.querySelector('.containerDataThree').classList.add('inactiveContainer');
+    document.querySelector('.containerDataOne').classList.remove('inactiveContainer');
+});
 
 document.querySelector('.modalSubmit').addEventListener('click', function (e) {
     e.preventDefault();
