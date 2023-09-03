8778a200ebc8727507535b5ab2b93152ee816803
rishi2025 <99127485+rishi2025@users.noreply.github.com>
2023-09-03 22:41:37 +0530

    Scroll Functionalities added

--- script.js
+++ script.js
@@ -2,6 +2,10 @@
 
 const overlay = document.querySelector('.overlay');
 const modal = document.querySelector('.modal');
+const nav = document.querySelector('.navBar');
+const header = document.querySelector('.header');
+const navHeight = nav.getBoundingClientRect().height;
+const section = document.querySelectorAll('.section');
 
 const openModal = function () {
     overlay.classList.remove('hidden');
@@ -23,62 +27,117 @@ const closePopUp = [document.querySelector('.modalClose'), document.querySelecto
 openPopUp.forEach(btn => btn.addEventListener('click', openModal));
 closePopUp.forEach(btn => btn.addEventListener('click', closeModal))
 
+const tabMenu = [...document.querySelector('.operationsTab').children];
 
+document.querySelector('.operationsTab').addEventListener('click', function (e) {
+    const clicked = e.target.closest('.operationButton');
 
+    if (!clicked) return;
 
+    tabMenu.forEach(t => t.classList.remove('activeButton'));
+    clicked.classList.add('activeButton');
 
+    const containerData = [...document.querySelector('.container').children];
+    containerData.forEach(e => e.classList.add('inactiveContainer'));
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-document.querySelector('.buttonTwo').addEventListener('click', function () {
-    console.log("clicked");
-    document.querySelector('.buttonTwo').classList.remove('activeButton');
-    document.querySelector('.buttonOne').classList.remove('activeButton');
-    document.querySelector('.buttonThree').classList.remove('activeButton');
-    document.querySelector('.buttonTwo').classList.add('activeButton');
-    document.querySelector('.containerDataTwo').classList.add('inactiveContainer');
-    document.querySelector('.containerDataOne').classList.add('inactiveContainer');
-    document.querySelector('.containerDataThree').classList.add('inactiveContainer');
-    document.querySelector('.containerDataTwo').classList.remove('inactiveContainer');
-});
-
-document.querySelector('.buttonThree').addEventListener('click', function () {
-    console.log("clicked");
-    document.querySelector('.buttonTwo').classList.remove('activeButton');
-    document.querySelector('.buttonOne').classList.remove('activeButton');
-    document.querySelector('.buttonThree').classList.remove('activeButton');
-    document.querySelector('.buttonThree').classList.add('activeButton');
-    document.querySelector('.containerDataTwo').classList.add('inactiveContainer');
-    document.querySelector('.containerDataOne').classList.add('inactiveContainer');
-    document.querySelector('.containerDataThree').classList.add('inactiveContainer');
-    document.querySelector('.containerDataThree').classList.remove('inactiveContainer');
-});
-
-document.querySelector('.buttonOne').addEventListener('click', function () {
-    console.log("clicked");
-    document.querySelector('.buttonTwo').classList.remove('activeButton');
-    document.querySelector('.buttonOne').classList.remove('activeButton');
-    document.querySelector('.buttonThree').classList.remove('activeButton');
-    document.querySelector('.buttonOne').classList.add('activeButton');
-    document.querySelector('.containerDataTwo').classList.add('inactiveContainer');
-    document.querySelector('.containerDataOne').classList.add('inactiveContainer');
-    document.querySelector('.containerDataThree').classList.add('inactiveContainer');
-    document.querySelector('.containerDataOne').classList.remove('inactiveContainer');
+    document.querySelector('.containerCircle').classList.remove('inactiveContainer');
+    document.querySelector(`.containerData${clicked.dataset.tab}`).classList.remove('inactiveContainer');
 });
 
 document.querySelector('.modalSubmit').addEventListener('click', function (e) {
     e.preventDefault();
 });
+
+const setOpacity = function (e, opacity) {
+    const active = e.target;
+        const siblings = nav.querySelectorAll('.navItem');
+        const logo = document.querySelector('.navLeft');
+        const openAccount = document.querySelector('.openAccount');
+
+        siblings.forEach(el => {
+            if (el !== active)
+                el.style.opacity = opacity;
+        });
+
+        logo.style.opacity = opacity;
+        openAccount.style.opacity = opacity;
+}
+
+nav.addEventListener('mouseover', function (e) {
+    if (e.target.classList.contains('navItem')) {
+        setOpacity(e, 0.5);
+    }
+});
+
+nav.addEventListener('mouseout', function (e) {
+    if (e.target.classList.contains('navItem')) {
+        setOpacity(e, 1);
+    }
+});
+
+//Sticky navigation Bar
+const stickyNav = function (entries) {
+    const [entry] = entries;
+    // console.log(entry);
+
+    if (!entry.isIntersecting)
+        nav.classList.add('navSticky');
+    else
+        nav.classList.remove('navSticky');
+};
+
+const headerObserver = new IntersectionObserver(stickyNav, {
+    root: null,
+    threshold: 0,
+    rootMargin: `-${navHeight}px`,
+});
+
+headerObserver.observe(header);
+
+// Ease in Components when scrolled to section
+const revealSection = function(entries, observer) {
+    const [entry] = entries;
+    // console.log(entry);
+
+    if (!entry.isIntersecting)
+        return;
+
+    entry.target.classList.remove('section');
+    observer.unobserve(entry.target);
+}
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+    root: null,
+    threshold: 0.05,
+});
+
+section.forEach(function (sec) {
+    sectionObserver.observe(sec);
+    sec.classList.add('section');
+});
+
+// Loading lazy Images
+const imgTargets = document.querySelectorAll('img[data-src]');
+
+const loadImg = function (entries, observer) {
+    const [entry] = entries;
+
+    if (!entry.isIntersecting)
+        return;
+
+    //Replace src with data src
+    entry.target.src = entry.target.dataset.src;
+
+    entry.target.addEventListener('load', function () {
+        entry.target.classList.remove('lazyImg');
+    });
+    observer.unobserve(entry.target);
+};
+
+const imgObserver = new IntersectionObserver(loadImg, {
+    root: null,
+    threshold: 0,
+    rootMargin: '200px',
+});
+
+imgTargets.forEach(img => imgObserver.observe(img));
